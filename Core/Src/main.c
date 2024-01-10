8ca88b00deb56220a90fa4c72aff3255c8a4ccf3
skedzier <szymon.kedzierski2.stud@pw.edu.pl>
2024-01-11 00:53:19 +0100

    add example for polling bm

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -27,7 +27,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "stdio.h"
-#include "vl6180_api.h"
+#include "vl53l0x_api.h"
 #include "mpu6050.h"
 /* USER CODE END Includes */
 
@@ -104,29 +104,61 @@ int main(void)
   MX_TIM3_Init();
   MX_USART6_UART_Init();
   /* USER CODE BEGIN 2 */
+  VL53L0X_RangingMeasurementData_t RangingData;
+  VL53L0X_Dev_t  vl53l0x_c; // center module
+  VL53L0X_DEV    Dev = &vl53l0x_c;
+  	//
+	// VL53L0X initialisation stuff
+	//
+    uint32_t refSpadCount;
+    uint8_t isApertureSpads;
+    uint8_t VhvSettings;
+    uint8_t PhaseCal;
+
+    Dev->I2cHandle = &hi2c1;
+    Dev->I2cDevAddr = 0x52;
+
+    HAL_GPIO_WritePin(VL6180_GPIO0_GPIO_Port, VL6180_GPIO0_Pin, GPIO_PIN_RESET); // Disable XSHUT
+    HAL_Delay(20);
+    HAL_GPIO_WritePin(VL6180_GPIO0_GPIO_Port, VL6180_GPIO0_Pin, GPIO_PIN_SET); // Enable XSHUT
+    HAL_Delay(20);
+
+    //
+    // VL53L0X init for Single Measurement
+    //
+
+    VL53L0X_WaitDeviceBooted( Dev );
+    VL53L0X_DataInit( Dev );
+    VL53L0X_StaticInit( Dev );
+    VL53L0X_PerformRefCalibration(Dev, &VhvSettings, &PhaseCal);
+    VL53L0X_PerformRefSpadManagement(Dev, &refSpadCount, &isApertureSpads);
+    VL53L0X_SetDeviceMode(Dev, VL53L0X_DEVICEMODE_SINGLE_RANGING);
+
+    // Enable/Disable Sigma and Signal check
+    VL53L0X_SetLimitCheckEnable(Dev, VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE, 1);
+    VL53L0X_SetLimitCheckEnable(Dev, VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, 1);
+    VL53L0X_SetLimitCheckValue(Dev, VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, (FixPoint1616_t)(0.1*65536));
+    VL53L0X_SetLimitCheckValue(Dev, VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE, (FixPoint1616_t)(60*65536));
+    VL53L0X_SetMeasurementTimingBudgetMicroSeconds(Dev, 33000);
+    VL53L0X_SetVcselPulsePeriod(Dev, VL53L0X_VCSEL_PERIOD_PRE_RANGE, 18);
+    VL53L0X_SetVcselPulsePeriod(Dev, VL53L0X_VCSEL_PERIOD_FINAL_RANGE, 14);
   /* USER CODE END 2 */
 
   /* Init scheduler */
-  osKernelInitialize();  /* Call init function for freertos objects (in freertos.c) */
-  MX_FREERTOS_Init();
+  //osKernelInitialize();  /* Call init function for freertos objects (in freertos.c) */
+  //MX_FREERTOS_Init();
 
   /* Start scheduler */
-  osKernelStart();
+  //osKernelStart();
 
   /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-   
-    test = MPU6050_GetRotationXRAW(&gyro_x_raw);
-    gyro_x_scaled = (float)gyro_x_raw * gyro_scale;
-    //HAL_Delay(50);
-    test = MPU6050_GetAccelerationXRAW(&accel_x_raw);
-    accel_x_scaled = (float)accel_x_raw * accel_scale;
-    //HAL_Delay(50);
-    //HAL_Delay(5000);
+    VL53L0X_PerformSingleRangingMeasurement(Dev, &RangingData);
+    if(RangingData.RangeStatus == 0)
+      printf("distance: %i mm\n\r", RangingData.RangeMilliMeter);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
