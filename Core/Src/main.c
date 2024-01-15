57df1b8519fb520f92b5e38858c618d0f7d23811
skedzier <szymon.kedzierski2.stud@pw.edu.pl>
2024-01-15 21:52:53 +0100

    start os

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -112,7 +112,7 @@ int main(void)
   MX_FREERTOS_Init();
 
   /* Start scheduler */
-  //osKernelStart();
+  osKernelStart();
 
   /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
